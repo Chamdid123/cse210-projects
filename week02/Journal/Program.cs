0cc2fcf18da6ebaa67af16295e6200465328e51d
Chamdid123 <muhammadchamdid@gmail.com>
2026-05-19 21:41:47 +0300

    Challenging

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Marketing Manager";
+        job2._company = "Google";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        job1.Display();
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Muhammad Noordin";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        Console.WriteLine();
+
+        myResume.Display();
+
+      
     }
 }
